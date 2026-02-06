5976a9d5e8e183559c1eda5a06228bddf3586d6e
sanskar225 <sanskarsinha225@gmail.com>
2026-02-07 02:14:19 +0530

    created leaderboard model

---
+++ backend/src/models/Leaderboard.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const LeaderboardSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    totalScore: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    leetCodeScore: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    githubScore: {
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    rank: {
+        type: Number,
+        default: 0
+    },
+    lastUpdated: {
+        type: Date,
+        default: Date.now
+    },
+    previousRank: {
+        type: Number,
+        default: 0
+    },
+    rankChange: {
+        type: Number,
+        default: 0
+    }
+});
+
+// Compound indexes for performance
+LeaderboardSchema.index({ totalScore: -1 });
+LeaderboardSchema.index({ rank: 1 });
+LeaderboardSchema.index({ user: 1 });
+LeaderboardSchema.index({ lastUpdated: -1 });
+
+LeaderboardSchema.pre('save', function(next) {
+    if (this.previousRank && this.rank) {
+        this.rankChange = this.previousRank - this.rank; // Positive = moved up
+    }
+    next();
+});
+
+module.exports = mongoose.model('Leaderboard', LeaderboardSchema);
