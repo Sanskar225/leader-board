f6b1bcf89d81a382510b757c48eaa7eefd36facb
sanskar225 <sanskarsinha225@gmail.com>
2026-02-07 02:13:01 +0530

    github model

---
+++ backend/src/models/GitHubStats.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const GitHubStatsSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    publicRepos: {
+        type: Number,
+        default: 0
+    },
+    totalStars: {
+        type: Number,
+        default: 0
+    },
+    totalForks: {
+        type: Number,
+        default: 0
+    },
+    followers: {
+        type: Number,
+        default: 0
+    },
+    following: {
+        type: Number,
+        default: 0
+    },
+    contributions: {
+        type: Number,
+        default: 0
+    },
+    avatar: {
+        type: String
+    },
+    profileUrl: {
+        type: String
+    },
+    lastSynced: {
+        type: Date,
+        default: Date.now
+    },
+    syncedAt: {
+        type: Date,
+        default: Date.now
+    },
+    rawData: {
+        type: mongoose.Schema.Types.Mixed,
+        default: {}
+    }
+});
+
+GitHubStatsSchema.index({ user: 1 });
+GitHubStatsSchema.index({ totalStars: -1 });
+GitHubStatsSchema.index({ contributions: -1 });
+
+module.exports = mongoose.model('GitHubStats', GitHubStatsSchema);
