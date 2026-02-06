1ae466e92ae789b13c95bdf7130b0f51ea34e031
sanskar225 <sanskarsinha225@gmail.com>
2026-02-07 02:13:42 +0530

    leetcode model created

---
+++ backend/src/models/LeetCodeStats.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const LeetCodeStatsSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    totalSolved: {
+        type: Number,
+        default: 0
+    },
+    easySolved: {
+        type: Number,
+        default: 0
+    },
+    mediumSolved: {
+        type: Number,
+        default: 0
+    },
+    hardSolved: {
+        type: Number,
+        default: 0
+    },
+    acceptanceRate: {
+        type: Number,
+        default: 0
+    },
+    ranking: {
+        type: Number,
+        default: 0
+    },
+    reputation: {
+        type: Number,
+        default: 0
+    },
+    contributionPoints: {
+        type: Number,
+        default: 0
+    },
+    lastSynced: {
+        type: Date,
+        default: Date.now
+    },
+    syncedAt: {
+        type: Date,
+        default: Date.now
+    },
+    rawData: {
+        type: mongoose.Schema.Types.Mixed,
+        default: {}
+    }
+});
+
+LeetCodeStatsSchema.index({ user: 1 });
+LeetCodeStatsSchema.index({ totalSolved: -1 });
+LeetCodeStatsSchema.index({ ranking: 1 });
+
+module.exports = mongoose.model('LeetCodeStats', LeetCodeStatsSchema);
